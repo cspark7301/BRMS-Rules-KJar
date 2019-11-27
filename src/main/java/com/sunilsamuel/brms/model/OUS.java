dc660a272e6a88e73d0e31246e1233416ff362cc
Louis Park <luispark@gmail.com>
2019-11-27 14:25:34 -0500

    Test 6

---
+++ src/main/java/com/sunilsamuel/brms/model/OUS.java
@@ -0,0 +1,20 @@
+package com.sunilsamuel.brms.model;
+
+public class OUS {
+	private String organization;
+	private String unit;
+	private String subunit;
+	private boolean access;
+
+	public String getOrganization() { return this.organization; }
+	public void setOrganization(String organization) { this.organization = organization; }
+
+	public String getUnit() { return this.unit; }
+	public void setUnit(String unit) { this.unit = unit; }
+
+	public String getSubunit() { return this.subunit; }
+	public void setSubunit(String subunit) { this.subunit = subunit; }
+
+	public boolean getAccess() { return this.access; }
+	public void setAccess(boolean access) { this.access = access; }
+}
